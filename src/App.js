4db7b78a3c01c301b7630403fad51db43165457f
Gleibson Alves <alvesgleibson@gmail.com>
2021-09-13 14:31:45 -0300

    React with github api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import GitHubImage from './download.png';
+import { useState } from 'react';
 
 function App() {
+const [search, setSearch] = useState('');
+const [userData, setUserData] = useState();
+
+const handkeSubmit = (event) =>{
+  event.preventDefault();
+  fetch(`https://api.github.com/users/${search}`)
+  .then(response => response.json())
+  .then(userResponse => setUserData(userResponse));
+}
+console.log(userData);
+
+const handleChange = (event) =>{
+  setSearch(event.target.value)
+
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container text-center">
+      <h1 className="py-5 text-uppercase">Github profile</h1>
+       <form onSubmit ={handkeSubmit}>
+        <div className="form-group">
+          <div className="input-grup">
+            <input type="text"
+            className="form-control" 
+            required
+            value = {search}
+            onChange={handleChange}
+            />
+            <span className="input-group-btn">
+              <button type="submit" className="btn btn-success">
+                Search
+              </button>
+            </span>
+          </div>
+        </div>
+    </form>
+    <div className="py-5">
+      {!userData&&(
+    <img src={GitHubImage} className="responsive rounded-circle" alt="" height="200p" />
+    )}  
+    {userData && (
+      <div>
+          <img src={userData.avatar_url} className="responsive rounded-circle" alt="" height="200p" />
+          <h1 className="pt-3">
+          <a href = {userData.html_url} target ="_new">{userData.login}</a>
+          </h1>
+              
+                 <h3>{userData.location}</h3>
+        
+
+          <p></p>
+      </div>
+     )}
     </div>
+  </div>
   );
 }
 
